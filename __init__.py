48a955e526079d2e3865bc824e86f4221dcb0b76
fuzidict <112486862+fuzidict@users.noreply.github.com>
2024-06-19 17:32:45 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,19 +7,30 @@ bl_info = {
 import bpy
 from .LSM import (
     LSystemRule,
-    LSystemModifier,
+    LSystemModifierProperties,
     OBJECT_OT_AddLSystemRule,
+    OBJECT_OT_ApplyLSystemModifier,
 )
 
 def register():
     bpy.utils.register_class(LSystemRule)
-    bpy.utils.register_class(LSystemModifier)
+    bpy.utils.register_class(LSystemModifierProperties)
     bpy.utils.register_class(OBJECT_OT_AddLSystemRule)
+    bpy.utils.register_class(OBJECT_OT_ApplyLSystemModifier)
+    bpy.types.Object.lsystem_modifier = bpy.props.PointerProperty(type=LSystemModifierProperties)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(LSystemRule)
-    bpy.utils.unregister_class(LSystemModifier)
+    bpy.utils.unregister_class(LSystemModifierProperties)
     bpy.utils.unregister_class(OBJECT_OT_AddLSystemRule)
+    bpy.utils.unregister_class(OBJECT_OT_ApplyLSystemModifier)
+    del bpy.types.Object.lsystem_modifier
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+def menu_func(self, context):
+    self.layout.operator(OBJECT_OT_ApplyLSystemModifier.bl_idname)
 
 if __name__ == "__main__":
     register()
+
